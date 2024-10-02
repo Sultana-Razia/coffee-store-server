6316bf14efee79ffe2a82b47305eeebeb038050a
Sultana Razia <sultanarazia483@gmail.com>
2024-10-02 21:42:34 +0600

    Client side url updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
         'https://coffee-store-2580d.web.app'
     ],
     credentials: true,
-    optionSuccessStatus: 200
+    optionSuccessStatus: 200,
 }
 
 app.use(cors(corsOptions));
@@ -127,7 +127,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
